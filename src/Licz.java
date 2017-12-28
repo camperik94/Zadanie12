3628c32e4f88ced5801f75d4ab375b7f6cea2171
camperik94 <tomaszjach94@gmail.com>
2017-12-28 20:30:06 +0100

    Final

--- src/Licz.java
+++ src/Licz.java
@@ -1,4 +1,42 @@
 
 public class Licz {
+	private int number;
+	private int maks = 0;
+	private int mini = 9999;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int funkcja(int liczba) {
+		number = number + liczba;
+
+		return number;
+	}
+
+	public int maksimum(int liczba) {
+		if (liczba > maks) {
+			maks = liczba;
+
+		}
+		return maks;
+	}
 	
+	public int minimum(int liczba) {
+		if (liczba < mini) {
+			mini = liczba;
+
+		}
+		return mini;
+	}
+	
+	public double srednia(int suma, int licznik) {
+		
+		return (double)suma / (double)licznik;
+	}
 }
+
--- src/Zadanie12.java
+++ src/Zadanie12.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
 
 public class Zadanie12 {
 	public static void main(String[] args) {
+		Licz li = new Licz();
+		Scanner in = new Scanner(System.in);
+
+		int liczba = 1;
+		int min = 999999;
+		int max = 1;
+		int licznik = 0;
+		
+		while (liczba != 0) {
+			liczba = in.nextInt();
+			
+			
+			
+			if(liczba == 0) {
+				break;
+			}
+			licznik++;
+			max = li.maksimum(liczba);
+			min = li.minimum(liczba);
+			li.funkcja(liczba);
+		}
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(li.getNumber());
+		System.out.println(licznik);
+		System.out.println(li.srednia(li.getNumber(), licznik));
+		
 		
 	}
 }
